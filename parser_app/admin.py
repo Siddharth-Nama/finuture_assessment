2d073deacebe5151389914c48de7af12d3399981
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-06 15:07:32 +0530

    register models in admin

--- parser_app/admin.py
+++ parser_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import InsuranceDocument, ExtractedData
 
-# Register your models here.
+admin.site.register(InsuranceDocument)
+admin.site.register(ExtractedData)
