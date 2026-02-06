c4e4bfa011d08114960677d64ca5e25612d67d8b
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-02-06 13:52:28 +0530

    add document upload view

--- parser_app/views.py
+++ parser_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from .serializers import InsuranceDocumentSerializer
 
-# Create your views here.
+class DocumentUploadView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        serializer = InsuranceDocumentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
